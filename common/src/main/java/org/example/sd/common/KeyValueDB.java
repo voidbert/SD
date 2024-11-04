b69c084b51bbffcb198c1a0a99576f778d7986ea
sara <saralopes3001@gmail.com>
2024-11-04 11:57:36 +0000

    Add KeyValueDB interface

---
+++ common/src/main/java/org/example/sd/common/KeyValueDB.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2024 Carolina Pereira, Diogo Costa, Humberto Gomes, Sara Lopes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.example.sd.common;
+
+import java.util.Map;
+
+public interface KeyValueDB {
+    public void                put(String key, byte[] value);
+    public byte[]              get(String key);
+    public void                multiPut(Map<String, byte[]> pairs);
+    public Map<String, byte[]> multiGet(Set<String> keys);
+    public byte[]              getWhen(String key, String keyCond, byte[] valueCond);
+}
