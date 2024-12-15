9b78827b221203948cf999c57f87342b68649bef
voidbert <humbertogilgomes@protonmail.com>
2024-12-15 16:19:23 +0000

    Add initial tester entry point

--- tester/src/main/java/org/example/sd/tester/Tester.java
+++ tester/src/main/java/org/example/sd/tester/Tester.java
@@ -16,10 +16,39 @@
 
 package org.example.sd.tester;
 
-import org.example.sd.common.Common;
+import org.example.sd.common.KeyValueDB;
+
+import org.apache.commons.math3.distribution.AbstractIntegerDistribution;
+import org.apache.commons.math3.distribution.UniformIntegerDistribution;
+import org.apache.commons.math3.distribution.ZipfDistribution;
 
 public class Tester {
     public static void main(String[] args) {
-        System.out.println(Common.TESTER_MESSAGE);
+        // TODO - make configurable
+
+        KeyValueDB        empty     = null; // TODO - use actual database when merged
+        DatabasePopulator populator = new DatabasePopulator(empty, 128, 128, 8, 8);
+
+        OperationDistribution operationDistribution =
+            new OperationDistribution(0.4, 0.39, 0.1, 0.10, 0.01);
+        AbstractIntegerDistribution keyDistribution = new ZipfDistribution(populator.getNKeys(), 1);
+        AbstractIntegerDistribution valueDistribution =
+            new ZipfDistribution(populator.getNValues(), 1);
+        AbstractIntegerDistribution multiCountDistribution = new UniformIntegerDistribution(1, 2);
+
+        int nThreads            = 8;
+        int nOperations         = 10_000_000;
+        int nOperationBlockSize = 10000;
+
+        Test test = new Test(populator,
+                             operationDistribution,
+                             keyDistribution,
+                             valueDistribution,
+                             multiCountDistribution,
+                             nThreads,
+                             nOperations,
+                             nOperationBlockSize);
+
+        System.out.println(test.run());
     }
 }
