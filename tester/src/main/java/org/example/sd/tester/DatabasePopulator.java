4a6dc0ef1d3621c05f7924890875ba7f178e849c
voidbert <humbertogilgomes@protonmail.com>
2024-12-15 16:15:13 +0000

    Add initial database population

---
+++ tester/src/main/java/org/example/sd/tester/DatabasePopulator.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2024 Carolina Pereira, Diogo Costa, Humberto Gomes, Sara Lopes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.example.sd.tester;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.example.sd.common.KeyValueDB;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+public class DatabasePopulator {
+    private final KeyValueDB empty;
+    private final int        nKeys, nValues, keyLength, valueLength;
+    private final Random     random;
+
+    public DatabasePopulator(KeyValueDB empty,
+                             int        nKeys,
+                             int        nValues,
+                             int        keyLength,
+                             int        valueLength) {
+
+        this.empty       = (KeyValueDB) empty.clone();
+        this.nKeys       = nKeys;
+        this.nValues     = nValues;
+        this.keyLength   = keyLength;
+        this.valueLength = valueLength;
+        this.random      = new Random();
+    }
+
+    public DatabasePopulator(DatabasePopulator populator) {
+        this(populator.getEmpty(),
+             populator.getNKeys(),
+             populator.getNValues(),
+             populator.getKeyLength(),
+             populator.getValueLength());
+    }
+
+    private String[] generateValidKeys() {
+        Set<String> ret = new HashSet<String>();
+
+        while (ret.size() < this.nKeys) {
+            String        chars   = "ABCDEFGHIJKLMONQRSTUVWXYZabcdefghijklmonqrstuvwxyz0123456789";
+            StringBuilder builder = new StringBuilder();
+
+            while (builder.length() < this.keyLength)
+                builder.append(chars.charAt(this.random.nextInt(chars.length())));
+
+            ret.add(builder.toString());
+        }
+
+        return ret.stream().toArray(String[] ::new);
+    }
+
+    private byte[][] generateValidValues() {
+        // Use List for valid .equals()
+        Set<List<Byte>> ret = new HashSet<List<Byte>>();
+        while (ret.size() < this.nValues) {
+            List<Byte> value = new ArrayList<Byte>(this.nValues);
+            for (int i = 0; i < this.valueLength; ++i)
+                value.add((byte) this.random.nextInt());
+
+            ret.add(value);
+        }
+
+        return ret.stream()
+            .map(l -> ArrayUtils.toPrimitive(l.toArray(new Byte[0])))
+            .toArray(byte[][] ::new);
+    }
+
+    public PopulatedDatabase newDatabase() {
+        KeyValueDB database    = (KeyValueDB) this.empty.clone();
+        String[]   validKeys   = this.generateValidKeys();
+        byte[][]   validValues = this.generateValidValues();
+
+        Map<String, byte[]> databaseContents = Arrays.stream(validKeys).collect(
+            Collectors.toMap(k -> k, k -> validValues[this.random.nextInt(validValues.length)]));
+        database.multiPut(databaseContents);
+
+        return new PopulatedDatabase(database, validKeys, validValues);
+    }
+
+    private KeyValueDB getEmpty() {
+        return (KeyValueDB) this.empty.clone();
+    }
+
+    public int getNKeys() {
+        return this.nKeys;
+    }
+
+    public int getNValues() {
+        return this.nValues;
+    }
+
+    public int getKeyLength() {
+        return this.keyLength;
+    }
+
+    public int getValueLength() {
+        return this.valueLength;
+    }
+
+    public Object clone() {
+        return new DatabasePopulator(this);
+    }
+
+    public boolean equals(Object o) {
+        if (o == null || o.getClass() != this.getClass())
+            return false;
+
+        DatabasePopulator populator = (DatabasePopulator) o;
+        return this.empty.equals(populator.getEmpty()) && this.nKeys == populator.getNKeys() &&
+            this.nValues == populator.getNValues() && this.keyLength == populator.getKeyLength() &&
+            this.valueLength == populator.getValueLength();
+    }
+
+    public String toString() {
+        return String.format(
+            "DatabasePopulator(empty = %s, nKeys = %d, nValues = %d, keyLength = %d, valueLength = %d)",
+            this.empty.toString(),
+            this.nKeys,
+            this.nValues,
+            this.keyLength,
+            this.valueLength);
+    }
+}
---
+++ tester/src/main/java/org/example/sd/tester/PopulatedDatabase.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2024 Carolina Pereira, Diogo Costa, Humberto Gomes, Sara Lopes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.example.sd.tester;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.example.sd.common.KeyValueDB;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+public class PopulatedDatabase {
+    private final KeyValueDB database;
+    private final String[] validKeys;
+    private final byte[][] validValues;
+
+    public PopulatedDatabase(KeyValueDB database, String[] validKeys, byte[][] validValues) {
+        this.database    = database;
+        this.validKeys   = validKeys;
+        this.validValues = validValues;
+    }
+
+    public PopulatedDatabase(PopulatedDatabase database) {
+        this((KeyValueDB) database.getDatabase().clone(),
+             database.getValidKeys().clone(),
+             Arrays.stream(database.getValidValues()).map(byte[] ::clone).toArray(byte[][] ::new));
+    }
+
+    public KeyValueDB getDatabase() {
+        return this.database;
+    }
+
+    public String[] getValidKeys() {
+        return this.validKeys;
+    }
+
+    private Set<String> getValidKeysSet() {
+        return Arrays.stream(this.validKeys).collect(Collectors.toSet());
+    }
+
+    public byte[][] getValidValues() {
+        return this.validValues;
+    }
+
+    // Use List<Byte> for valid equals
+    private Set<List<Byte>> getValidValuesSet() {
+        return Arrays.stream(this.validValues)
+            .map(a -> Arrays.asList(ArrayUtils.toObject(a)))
+            .collect(Collectors.toSet());
+    }
+
+    public Object clone() {
+        return new PopulatedDatabase(this);
+    }
+
+    public boolean equals(Object o) {
+        if (o == null || o.getClass() != this.getClass())
+            return false;
+
+        PopulatedDatabase database = (PopulatedDatabase) o;
+        return this.database.equals(database.getDatabase()) &&
+            this.getValidKeysSet().equals(database.getValidKeysSet()) &&
+            this.getValidValuesSet().equals(database.getValidValuesSet());
+    }
+
+    public String toString() {
+        return String.format("PopulatedDatabase(database = %s, validKeys = %s, validValues = %s)",
+                             this.database.toString(),
+                             Arrays.toString(this.validKeys),
+                             Arrays.deepToString(this.validValues));
+    }
+}
