1ea40f4b881124741ff633d5f72de19903bdb3d4
voidbert <humbertogilgomes@protonmail.com>
2024-12-21 22:03:58 +0000

    Implement general message serialization

--- common/src/main/java/org/example/sd/common/Message.java
+++ common/src/main/java/org/example/sd/common/Message.java
@@ -19,16 +19,56 @@ package org.example.sd.common;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public abstract class Message {
+    private static Map<String, Byte> classToTypeInteger;
+    private static Map<Byte, String> typeIntegerToClass;
+
+    static {
+        Message.classToTypeInteger =
+            Map.ofEntries(Map.entry("RegisterAuthenticateRequestMessage", (byte) 0),
+                          Map.entry("RegisterAuthenticateResponseMessage", (byte) 1),
+
+                          Map.entry("PutRequestMessage", (byte) 2),
+                          Map.entry("GetRequestMessage", (byte) 3),
+                          Map.entry("MultiPutRequestMessage", (byte) 4),
+                          Map.entry("MultiGetRequestMessage", (byte) 5),
+                          Map.entry("GetWhenRequestMessage", (byte) 6),
+
+                          Map.entry("PutResponseMessage", (byte) 7),
+                          Map.entry("GetResponseMessage", (byte) 8),
+                          Map.entry("MultiPutResponseMessage", (byte) 9),
+                          Map.entry("MultiGetResponseMessage", (byte) 10));
+
+        Message.typeIntegerToClass = Message.classToTypeInteger.entrySet().stream().collect(
+            Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey));
+    }
+
     public static Message deserialize(DataInputStream in) throws IOException {
-        // TODO - implement Message.deserialize
-        throw new UnsupportedOperationException();
+        String messageClassName = Message.typeIntegerToClass.get(in.readByte());
+        if (messageClassName == null)
+            throw new RuntimeException("Message class not supported");
+
+        try {
+            Class<?> messageClass = Class.forName("org.example.sd.common." + messageClassName);
+            Method deserializeMethod = messageClass.getMethod("messageDeserialize", DataInputStream.class);
+            return (Message) deserializeMethod.invoke(null, in);
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void serialize(DataOutputStream out) throws IOException {
-        // TODO - implement Message.serialize
-        throw new UnsupportedOperationException();
+        Byte typeInteger = Message.classToTypeInteger.get(this.getClass().getSimpleName());
+        if (typeInteger == null)
+            throw new RuntimeException("Message class not supported");
+        
+        out.writeByte(typeInteger);
+        this.messageSerialize(out);
     }
 
     protected abstract void messageSerialize(DataOutputStream out) throws IOException;
