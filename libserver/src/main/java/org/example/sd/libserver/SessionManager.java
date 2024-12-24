453d8b8aea07e4b4c6ffaa3415819c41b46d13fe
voidbert <humbertogilgomes@protonmail.com>
2024-12-24 13:36:34 +0000

    Add passwords and login order to SessionManager

--- libserver/src/main/java/org/example/sd/libserver/SessionManager.java
+++ libserver/src/main/java/org/example/sd/libserver/SessionManager.java
@@ -16,73 +16,113 @@
 
 package org.example.sd.libserver;
 
-import java.net.Socket;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.Queue;
+import java.util.Set;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class SessionManager {
-    private final int maxSessions;
-    private final Queue<Socket> waitingQueue;
-    private final Map<String, Socket> activeClients;
-    private final ReentrantLock       lock;
-    private final Condition           sessionAvailable;
+    private final int           maxSessions;
+    private Map<String, String> passwords;
+    private Queue<String>       waitingQueue;
+    private Set<String>         activeClients;
+    private final ReentrantLock lock;
+    private final Condition     sessionAvailable;
 
     public SessionManager(int maxSessions) {
         this.maxSessions      = maxSessions;
-        this.waitingQueue     = new LinkedList<>();
-        this.activeClients    = new HashMap<>();
+        this.passwords        = new HashMap<String, String>();
+        this.waitingQueue     = new LinkedList<String>();
+        this.activeClients    = new HashSet<String>();
         this.lock             = new ReentrantLock();
         this.sessionAvailable = lock.newCondition();
     }
 
-    public Socket acquireSession(Socket clientSocket, String username)
-        throws InterruptedException, SessionException {
-        lock.lock();
+    public SessionManager(SessionManager manager) {
+        this(manager.getMaxSessions());
+        this.passwords = manager.getPasswords();
+    }
+
+    public boolean acquireSession(String username, String password) throws SessionException {
+        boolean newUser = false;
+
+        this.lock.lock();
         try {
-            if (activeClients.containsKey(username)) {
+            if (this.activeClients.contains(username))
                 throw new SessionException("User '" + username + "' is already authenticated.");
+
+            String truePassword = this.passwords.get(username);
+            if (truePassword == null) {
+                this.passwords.put(username, password);
+                newUser = true;
+            } else if (!truePassword.equals(password)) {
+                throw new SessionException("Wrong password");
             }
 
-            while (activeClients.size() >= maxSessions) {
-                if (!waitingQueue.contains(clientSocket)) {
-                    waitingQueue.add(clientSocket);
-                }
-                sessionAvailable.awaitUninterruptibly();
+            if (!this.waitingQueue.contains(username))
+                this.waitingQueue.add(username);
+
+            while (this.activeClients.size() >= this.maxSessions ||
+                   !username.equals(this.waitingQueue.peek())) {
+
+                this.sessionAvailable.awaitUninterruptibly();
             }
 
-            waitingQueue.remove(clientSocket);
-            activeClients.put(username, clientSocket);
-            return clientSocket;
+            this.waitingQueue.poll();
+            this.activeClients.add(username);
+            return newUser;
         } finally {
-            lock.unlock();
+            this.lock.unlock();
         }
     }
 
     public void releaseSession(String username) {
-        lock.lock();
+        this.lock.lock();
         try {
-            if (activeClients.containsKey(username)) {
-                activeClients.remove(username);
+            if (this.activeClients.contains(username)) {
+                this.activeClients.remove(username);
 
-                if (!waitingQueue.isEmpty()) {
-                    sessionAvailable.signal();
-                }
+                if (!this.waitingQueue.isEmpty())
+                    this.sessionAvailable.signalAll();
             }
         } finally {
-            lock.unlock();
+            this.lock.unlock();
         }
     }
 
-    public int getActiveSessions() {
-        lock.lock();
+    public int getMaxSessions() {
+        return this.maxSessions;
+    }
+
+    public Map<String, String> getPasswords() {
+        this.lock.lock();
         try {
-            return activeClients.size();
+            return new HashMap<String, String>(this.passwords);
         } finally {
-            lock.unlock();
+            this.lock.unlock();
         }
+    }
+
+    public Object clone() {
+        return new SessionManager(this);
+    }
+
+    public boolean equals(Object o) {
+        if (o == null || o.getClass() != this.getClass())
+            return false;
+
+        SessionManager sessions = (SessionManager) o;
+        return this.maxSessions == sessions.getMaxSessions() &&
+            this.passwords.equals(sessions.getPasswords());
+    }
+
+    public String toString() {
+        return String.format("SessionManager(maxSessions=%d, passwords=%s)",
+                             this.maxSessions,
+                             this.passwords.toString());
     }
 }
